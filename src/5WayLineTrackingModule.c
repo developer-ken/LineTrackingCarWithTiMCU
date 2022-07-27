7985036076cc575ec01d0b93964e4fe166ce41c4
dengbw01 <dengbw01@outlook.com>
2022-07-27 23:02:00 +0800

    基本循迹逻辑(待验证)

---
+++ src/5WayLineTrackingModule.c
@@ -0,0 +1,110 @@
+#include "5WayLineTrackingModule.h"
+StateCommand CurrentState = NoState;
+
+StateCommand LineTrackingScan(PinConfig5Way pinConfig, Loop loop)
+{
+    pinMode(pinConfig.Left, INPUT);
+    pinMode(pinConfig.LMiddle, INPUT);
+    pinMode(pinConfig.Middle, INPUT);
+    pinMode(pinConfig.RMiddle, INPUT);
+    pinMode(pinConfig.Right, INPUT);
+
+    uint8_t left = digitalRead(pinConfig.Left);
+    uint8_t lmiddle = digitalRead(pinConfig.LMiddle);
+    uint8_t middle = digitalRead(pinConfig.Middle);
+    uint8_t rmiddle = digitalRead(pinConfig.RMiddle);
+    uint8_t right = digitalRead(pinConfig.Right);
+
+    // 基本巡线逻辑 —— 单传感器压线
+    if (left == pinConfig.TriggerLevel &&
+        lmiddle != pinConfig.TriggerLevel &&
+        middle != pinConfig.TriggerLevel &&
+        rmiddle != pinConfig.TriggerLevel &&
+        right != pinConfig.TriggerLevel) // ■□□□□
+    {
+        CurrentState = RapidLeft;
+    }
+    else if (left != pinConfig.TriggerLevel &&
+             lmiddle != pinConfig.TriggerLevel &&
+             middle != pinConfig.TriggerLevel &&
+             rmiddle != pinConfig.TriggerLevel &&
+             right == pinConfig.TriggerLevel) // □□□□■
+    {
+        CurrentState = RapidRight;
+    }
+    else if (left != pinConfig.TriggerLevel &&
+             lmiddle == pinConfig.TriggerLevel &&
+             rmiddle != pinConfig.TriggerLevel &&
+             right != pinConfig.TriggerLevel &&
+             CurrentState != RapidLeft &&
+             CurrentState != RapidRight) // □■X□□
+    {
+        CurrentState = Left;
+    }
+    else if (left != pinConfig.TriggerLevel &&
+             lmiddle != pinConfig.TriggerLevel &&
+             rmiddle == pinConfig.TriggerLevel &&
+             right != pinConfig.TriggerLevel &&
+             CurrentState != RapidLeft &&
+             CurrentState != RapidRight) // □□X■□
+    {
+        CurrentState = Right;
+    }
+    else if (left != pinConfig.TriggerLevel &&
+             lmiddle != pinConfig.TriggerLevel &&
+             middle == pinConfig.TriggerLevel &&
+             rmiddle != pinConfig.TriggerLevel &&
+             right != pinConfig.TriggerLevel) // □□■□□
+    {
+        CurrentState = Straight;
+    }
+
+    // 特殊情况 —— 多传感器压线或无传感器压线
+    else if (left == pinConfig.TriggerLevel &&
+             lmiddle == pinConfig.TriggerLevel &&
+             middle == pinConfig.TriggerLevel &&
+             rmiddle == pinConfig.TriggerLevel &&
+             right == pinConfig.TriggerLevel) // ■■■■■
+    {
+        CurrentState = CheckPoint;
+    }
+    else if (left != pinConfig.TriggerLevel &&
+             lmiddle != pinConfig.TriggerLevel &&
+             middle != pinConfig.TriggerLevel &&
+             rmiddle != pinConfig.TriggerLevel &&
+             right != pinConfig.TriggerLevel &&
+             CurrentState != RapidLeft &&
+             CurrentState != RapidRight) // □□□□□
+    {
+        CurrentState = Pause_Straight_Loss;
+    }
+    else if (left == pinConfig.TriggerLevel &&
+             (lmiddle == pinConfig.TriggerLevel ||
+              middle == pinConfig.TriggerLevel ||
+              rmiddle == pinConfig.TriggerLevel) &&
+             right != pinConfig.TriggerLevel) // ■+++□  岔路
+    {
+        if (loop == Inner)
+        {
+            CurrentState = RapidLeft;
+        }
+        else
+        {
+            CurrentState = Straight;
+        }
+    }else if (left != pinConfig.TriggerLevel &&
+             (lmiddle == pinConfig.TriggerLevel ||
+              middle == pinConfig.TriggerLevel ||
+              rmiddle == pinConfig.TriggerLevel) &&
+             right == pinConfig.TriggerLevel) // □+++■  岔路
+    {
+        if (loop == Inner)
+        {
+            CurrentState = RapidRight;
+        }
+        else
+        {
+            CurrentState = Straight;
+        }
+    }
+}
---
+++ src/5WayLineTrackingModule.h
@@ -0,0 +1,32 @@
+#ifndef _5WAYLINETRACKINGMODULE_H
+#define _5WAYLINETRACKINGMODULE_H
+#include <Arduino.h>
+#include <stdint.h>
+
+struct
+{
+    uint8_t Left, LMiddle, Middle, RMiddle, Right; // Pin numbers
+    uint8_t TriggerLevel;// HIGH or LOW
+} typedef PinConfig5Way;
+
+enum
+{
+    Inner = 0,
+    Outter = 1
+} typedef Loop;
+
+enum
+{
+    RapidLeft = -2,
+    Left = -1,
+    Straight = 0,
+    Right = 1,
+    RapidRight = 2,
+
+    CheckPoint = 3,
+    Pause_Straight_Loss = 4,
+    NoState = 10
+} typedef StateCommand;
+
+StateCommand LineTrackingScan(PinConfig5Way);
+#endif
