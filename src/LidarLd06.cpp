0539796ba34faa61b67c6753a4a9767aa78bdf63
dengbw01 <dengbw01@outlook.com>
2022-07-29 18:29:14 +0800

    增加激光雷达测距相关代码

--- src/LidarLd06.cpp
+++ src/LidarLd06.cpp
@@ -15,8 +15,10 @@ void LidarLd06::begin()
 
 void LidarLd06::update()
 {
+    // Logger::Log("u");
     ldlidar->read_lidar_data();
-    ClosestPoint = PoleVector2d(0, 65535);
+    if (ldlidar->angles[0] < 180 && ldlidar->angles[ldlidar->angles.size() - 1] > 180.0)
+        ClosestPoint = PoleVector2d(500, 65535);
     for (int i = 0; i < POINT_PER_PACK; i++)
     {
         double len = ldlidar->distances[i];
@@ -24,7 +26,7 @@ void LidarLd06::update()
         // Logger::Log("----------distance:" + String(len));
         if (len > 0)
         {
-            // Logger::Log("distance:" + String(len));
+            //    Logger::Log("d" + String(len));
             PoleVector2d point;
             PoleVector2d pv2d = PoleVector2d(ang, len);
             AddPoint(pv2d);
@@ -36,7 +38,8 @@ void LidarLd06::AddPoint(PoleVector2d pv2d)
 {
     pcloudpointer = (1 + pcloudpointer) % 360;
     pointCloud[pcloudpointer] = pv2d;
-    if (ClosestPoint.Distance > pv2d.Distance || ClosestPoint.Distance < 0.1)
+    if (ClosestPoint.Distance > pv2d.Distance && pv2d.Distance > 0.1 &&
+        pv2d.Angle >= 200 && pv2d.Angle <= 320)
     {
         ClosestPoint = pv2d;
     }
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <arduino-timer.h>
 #include "LidarLd06.h"
 #include "Vectors.hpp"
 #include "F5WayLineTrackingModule.h"
@@ -6,12 +7,16 @@
 #include "UpperStateMachine.h"
 #include "Encoder.h"
 #include "Motor.h"
+#include "Logger.h"
+
+#define MAX_BASE_SPEED 90
 
 //#define AUTO_TUNE_PID
 
 LidarLd06 lidar;
+Timer<10, millis> timer;
 Motor motorL(MotorConfig{39, 38, 37, 6, 7, 1, 0.1, 0.1, true});
-Motor motorR(MotorConfig{34, 35, 36, 3, 5, 1, 0.1, 0.1, true});
+Motor motorR(MotorConfig{34, 35, 36, 4, 5, 1, 0.1, 0.1, true});
 PinConfig5Way linetrackingCfg = {
     .Left = 26,
     .LMiddle = 27,
@@ -22,62 +27,105 @@ PinConfig5Way linetrackingCfg = {
 UpperStateMachine statemachine(linetrackingCfg);
 
 #ifndef AUTO_TUNE_PID
+
+bool update(void *argument);
+bool update_lidar(void *argument);
+double closest_distance = INFINITY; // Deg = 200~320
+
 void setup()
 {
+  Serial.begin(115200);
   Serial1.begin(230400);
-  // lidar.begin(Serial1, 230400);
+  Serial.println("20220729 by dev_ken | booting...");
+  Logger::serial = &Serial;
+  // pinMode(PB_2, OUTPUT);
+  lidar.begin();
   // motorL.Rotate(3);
   // motorR.Rotate(3);
+  timer.every(100, update);
+  timer.every(5, update_lidar);
 }
 
 StateCommand last;
 uint8_t loopnum = 0;
+uint8_t basespeed = 70;
+
+bool update(void *argument)
+{
+  if (lidar.ClosestPoint.Angle >= 200 && lidar.ClosestPoint.Angle <= 320)
+  { //仅由前方触发  此判断是冗余的，在LidarLd06中也有相关过滤器
+    closest_distance = lidar.ClosestPoint.Distance;
+  }
+  else
+  {
+    closest_distance = INFINITY;
+  }
+  Logger::Log(String(lidar.ClosestPoint.Angle) + "->" + String(lidar.ClosestPoint.Distance));
+  if(closest_distance <185){
+    basespeed = 0;
+    last = NoState;
+  }
+  if (closest_distance < 200 && basespeed > 0)
+  {
+    basespeed--;
+    last = NoState;
+  }
+  else if (closest_distance > 200 && basespeed < MAX_BASE_SPEED)
+  {
+    basespeed++;
+    last = NoState;
+  }
+
+
+  return true;
+}
+
+bool update_lidar(void *argument)
+{
+  lidar.update();
+  return true;
+}
 
 void loop()
 {
+  timer.tick();
   StateCommand cmd = statemachine.tracker.LineTrackingScan(linetrackingCfg, (Loop)(loopnum % 2));
-  // if (loopnum >= 4)
-  //{
-  //   motorL.RunPwm(0);
-  //   motorR.RunPwm(0);
-  //   return;
-  // }
   if (last != cmd)
     switch (cmd)
     {
     case Left:
-      motorL.RunPwm(60);
-      motorR.RunPwm(90);
+      motorL.RunPwm(basespeed - 20);
+      motorR.RunPwm(basespeed + 10);
       break;
     case Right:
-      motorL.RunPwm(90);
-      motorR.RunPwm(60);
+      motorL.RunPwm(basespeed + 10);
+      motorR.RunPwm(basespeed - 20);
       break;
     case RapidLeft:
       motorL.RunPwm(10);
-      motorR.RunPwm(90);
+      motorR.RunPwm(basespeed + 10);
+      digitalWrite(PB_2, 0);
       break;
     case RapidRight:
-      motorL.RunPwm(90);
+      motorL.RunPwm(basespeed + 10);
       motorR.RunPwm(10);
       break;
     case Pause:
       motorL.RunPwm(0);
       motorR.RunPwm(0);
       delay(3000);
-      motorL.RunPwm(90);
-      motorR.RunPwm(90);
+      motorL.RunPwm(basespeed + 10);
+      motorR.RunPwm(basespeed + 10);
     case CheckPoint:
       loopnum++;
+      analogWrite(PB_2, 255 / 2);
     case Straight:
     default:
-      motorL.RunPwm(90);
-      motorR.RunPwm(90);
+      motorL.RunPwm(basespeed + 10);
+      motorR.RunPwm(basespeed + 10);
       break;
     }
   last = cmd;
-  // motorL.update();
-  // motorR.update();
 }
 #endif
 
